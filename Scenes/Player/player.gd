fa38120bf95c667c29aacc3a11475e1393f28ba7
George Whittington <lwhittington2231@gmail.com>
2025-04-09 23:50:53 -0400

    Added Player to GameScene also made the Player floating instead of grounded

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-@export var move_speed: int = 100
+@export var move_speed: int = 5000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
