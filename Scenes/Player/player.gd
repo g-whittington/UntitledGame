739a1ff1ed1d30a0f0fa6a1b3c2f077a399e935c
George Whittington <lwhittington2231@gmail.com>
2025-04-10 00:17:30 -0400

    My Keybaord is wack

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-@export var move_speed: int = 5000
+@export var move_speed: int = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,7 +14,7 @@ func _process(delta: float) -> void:
 	var movement: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	# velocity is built in variable
-	velocity = movement * move_speed * delta
+	velocity = movement * move_speed
 	
 	# update player position
 	move_and_slide()
