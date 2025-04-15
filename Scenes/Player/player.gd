432cabab4e0bc281686151bac4aeac1a0403fd84
George Whittington <lwhittington2231@gmail.com>
2025-04-15 01:59:33 -0400

    Created an Autoload Node that currently stores the position of the where the Player should spawn when changing scenes

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -8,7 +8,7 @@ class_name Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass 
+	position = SceneManager.player_spawn_position 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
